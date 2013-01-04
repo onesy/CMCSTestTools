950b13694ef0a35ef94890faa59495e5989ef4f8
onesy <leentingOne@gmail.com>
2013-01-04 16:34:33 +0800

    want more

---
+++ src/org/CMCSToolsSet/main/MainPower.java
@@ -0,0 +1,13 @@
+package org.CMCSToolsSet.main;
+
+public class MainPower {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
