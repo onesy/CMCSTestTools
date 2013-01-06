c9be3691b5e634350a56320a17a82a06c5d5378c
onesy <leentingOne@gmail.com>
2013-01-06 22:20:44 +0800

    begin to work

--- src/org/CMCSToolsSet/main/MainPower.java
+++ src/org/CMCSToolsSet/main/MainPower.java
@@ -11,7 +11,7 @@ public class MainPower {
 		// TODO Auto-generated method stub
 		TestConfirm testConfirm = new TestConfirm();
 		testConfirm.process();
-		现在还不能像指定端口指定库发送消息，明日之内完成，白天记得要些文档
+		// 现在还不能像指定端口指定库发送消息，明日之内完成，白天记得要些文档
 	}
 
 }
